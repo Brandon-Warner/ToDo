50586fab0948251979b9bf967f1a3f5324a88d8d
Brandon Warner <brandonswarner5@gmail.com>
2021-12-06 13:19:15 -0800

    items can be added to list & checking items will toggle their completed state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return (
-    <div>
-      <h1>ToDo Checklist</h1>
-    </div>
-  );
-}
+    const [listItem, setListItem] = useState('');
+    const [list, setList] = useState([]);
+    console.log('listItem: ', listItem);
+    console.log('list: ', list);
+
+    const submit = e => {
+        e.preventDefault();
+
+        const newListItem = {
+            id: Math.random().toString(),
+            content: listItem,
+            completed: false
+        };
+
+        const newList = [...list, newListItem];
+        setList(newList);
+    };
+
+    const toggleCompleted = id => {
+        const newList = [...list];
+        const itemToChange = newList.find(i => i.id === id);
+        itemToChange.completed = !itemToChange.completed;
+        console.log('itemToChange: ', itemToChange);
+
+        const listWithFoundItemReplaced = newList.map(i => i.id === id ? itemToChange : i);
+        console.log('listWithFoundItemReplaced: ', listWithFoundItemReplaced);
+
+        setList(listWithFoundItemReplaced);
+    };
+    return (
+        <div>
+            <h1>ToDo Checklist</h1>
+
+            <form onSubmit={submit}>
+                <input type='text' onChange={e => setListItem(e.target.value)} />
+                <button type='submit'>add item</button>
+            </form>
+
+            <div>
+                <ul>
+                    {list.map(l => (
+                        <li key={l.id}>
+                            {l.content}
+                            <input
+                                type='checkbox'
+                                checked={l.completed}
+                                onChange={() => toggleCompleted(l.id)}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
 
 export default App;
