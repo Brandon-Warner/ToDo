21992115fd4d843b1138856949f991c2a2bd8a46
Brandon Warner <brandonswarner5@gmail.com>
2021-12-06 13:45:43 -0800

    delete option conditional of checkbox checked or not

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
         };
 
         const newList = [...list, newListItem];
+        setListItem('');
         setList(newList);
     };
 
@@ -25,17 +26,26 @@ const App = () => {
         itemToChange.completed = !itemToChange.completed;
         console.log('itemToChange: ', itemToChange);
 
-        const listWithFoundItemReplaced = newList.map(i => i.id === id ? itemToChange : i);
+        const listWithFoundItemReplaced = newList.map(i => (i.id === id ? itemToChange : i));
         console.log('listWithFoundItemReplaced: ', listWithFoundItemReplaced);
 
         setList(listWithFoundItemReplaced);
     };
+
+    const removeItem = id => {
+        const newList = [...list];
+        const listWithItemRemoved = newList.filter(i => i.id !== id);
+        console.log('listWithItemRemoved: ', listWithItemRemoved);
+
+        setList(listWithItemRemoved);
+    };
+
     return (
         <div>
             <h1>ToDo Checklist</h1>
 
             <form onSubmit={submit}>
-                <input type='text' onChange={e => setListItem(e.target.value)} />
+                <input type='text' value={listItem} onChange={e => setListItem(e.target.value)} />
                 <button type='submit'>add item</button>
             </form>
 
@@ -49,6 +59,9 @@ const App = () => {
                                 checked={l.completed}
                                 onChange={() => toggleCompleted(l.id)}
                             />
+                            {l.completed ? (
+                                <button onClick={() => removeItem(l.id)}>delete</button>
+                            ) : null}
                         </li>
                     ))}
                 </ul>
